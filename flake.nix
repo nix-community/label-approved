0fee9fd543409da3e0c9046aae8f1b1a6e8b031c
Pierre Bourdon <delroth@gmail.com>
2023-09-08 16:49:36 +0200

    flake: add a basic nixosModule

--- flake.nix
+++ flake.nix
@@ -72,5 +72,44 @@
           };
         };
 
-      });
+      }) // {
+        nixosModule = { config, lib, pkgs, ... }: let
+          cfg = config.services.label-approved;
+        in {
+          options.services.label-approved = with lib; {
+            enable = mkEnableOption "Enables the approved PRs labeler service.";
+            interval = mkOption {
+              default = "*:0/30";
+              type = types.str;
+              description = lib.mdDoc "systemd-timer OnCalendar config";
+            };
+            environmentFile = mkOption {
+              type = types.path;
+              example = "/run/secrets/label-approved.env";
+              description = lib.mdDoc ''
+                Environment file to source before running the service. This
+                should contain a GITHUB_TOKEN or GITHUB_BOT_TOKEN variable.
+              '';
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.timers.label-approved = {
+              wantedBy = [ "timers.target" ];
+              after = [ "multi-user.target" ];
+              timerConfig.OnCalendar = cfg.interval;
+            };
+            systemd.services.label-approved = {
+              description = "label-approved service";
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              serviceConfig = {
+                DynamicUser = true;
+                EnvironmentFile = cfg.environmentFile;
+                ExecStart = "${self.packages.${pkgs.system}.label-approved}/bin/label-approved";
+              };
+            };
+          };
+        };
+      };
 }
